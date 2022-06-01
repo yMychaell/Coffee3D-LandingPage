af62221134ab56a4058cc51f06725dcb8278be2c
Mychael Dourado <mychaeldourado23@gmail.com>
2022-06-01 19:17:41 -0300

    Creating animations with gsap lib.

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,12 @@ function moveEffect(e) {
     element.style.transform = `translateX(${x}px) translateY(${y}px)`
   });
 }
+
+gsap.from('.nav__logo', {opacity: 0, duration:1, delay:1, y:10})
+gsap.from('.nav__item', {opacity: 0, duration:1, delay:1.4, y:30, stagger: .2})
+
+gsap.from('.home__title', {opacity: 0, duration:1, delay:1, x:'-100vw' })
+gsap.from('.home__description', {opacity: 0, duration:1, delay:1.3, y:30})
+gsap.from('.home__button', {opacity: 0, duration:1, delay:1.6, y:30})
+gsap.from('.home__img', {opacity: 0, duration:1, delay:1.3, y:30})
+
