734dd5942a664952113e4715fa9d19af46f5956a
Mychael Dourado <mychaeldourado23@gmail.com>
2022-06-01 19:02:24 -0300

    Creating the 3d effect on images.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 navToggle.addEventListener('click', () => {
 
@@ -14,4 +14,17 @@ navClose.addEventListener('click', () => {
 
 });
 
+document.addEventListener('mousemove', moveEffect);
 
+function moveEffect(e) {
+  this.querySelectorAll('.move').forEach(element => {
+    const speed = element.getAttribute('data-speed');
+
+    console.log(element)
+
+    const x = (window.innerWidth - e.pageX * speed) / 120
+    const y = (window.innerHeight - e.pageY * speed) / 120
+
+    element.style.transform = `translateX(${x}px) translateY(${y}px)`
+  });
+}
